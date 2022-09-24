276b88f952bd8586e960156d2e50db01fe7b9228
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2022-09-24 16:14:47 +0900

    changes bytes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 
-
 fn main() {
     println!("Hello, world!");
 }
@@ -11,9 +10,9 @@ pub struct KVStore {
 
 trait KVService {
     fn new() -> Self;
-    fn put(&mut self, key: &[u8], value: &[u8]);
-    fn get(&self, key: &[u8]) -> Option<&[u8]>;
-    fn delete(&mut self, key: &[u8]);
+    fn put(&mut self, key: Vec<u8>, value: Vec<u8>);
+    fn get(&self, key: Vec<u8>) -> Option<Vec<u8>>;
+    fn delete(&mut self, key: Vec<u8>);
 }
 
 impl KVService for KVStore {
@@ -23,15 +22,42 @@ impl KVService for KVStore {
         }
     }
 
-    fn put(&mut self, key: &[u8], value: &[u8]) {
-        self.map.insert(String::from_utf8(key.to_vec()).unwrap(), String::from_utf8(value.to_vec()).unwrap());
+    fn put(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        self.map.insert(String::from_utf8(key).unwrap(), String::from_utf8(value).unwrap());
     }
 
-    fn get(&self, key: &[u8]) -> Option<&[u8]> {
-        self.map.get(&String::from_utf8(key.to_vec()).unwrap()).map(|s| s.as_bytes())
+    fn get(&self, key: Vec<u8>) -> Option<Vec<u8>> {
+        self.map.get(&String::from_utf8(key).unwrap()).map(|v| v.as_bytes().to_vec())
     }
 
-    fn delete(&mut self, key: &[u8]) {
-        self.map.remove(&String::from_utf8(key.to_vec()).unwrap());
+    fn delete(&mut self, key: Vec<u8>) {
+        self.map.remove(&String::from_utf8(key).unwrap());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put() {
+        let mut store = KVStore::new();
+        store.put("key".as_bytes().to_vec(), "value".as_bytes().to_vec());
+        assert_eq!(store.get("key".as_bytes().to_vec()), Some("value".as_bytes().to_vec()));
+    }
+
+    #[test]
+    fn test_get() {
+        let mut store = KVStore::new();
+        store.put("key".as_bytes().to_vec(), "value".as_bytes().to_vec());
+        assert_eq!(store.get("key".as_bytes().to_vec()), Some("value".as_bytes().to_vec()));
+    }
+
+    #[test]
+    fn test_delete() {
+        let mut store = KVStore::new();
+        store.put("key".as_bytes().to_vec(), "value".as_bytes().to_vec());
+        store.delete("key".as_bytes().to_vec());
+        assert_eq!(store.get("key".as_bytes().to_vec()), None);
     }
 }
