5aae8b7ea456298abaea3f55d78c81a7f4f63756
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2022-09-24 23:54:49 +0900

    PersistentStore

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
 use std::collections::HashMap;
+use std::os;
 
-pub struct KVStore {
+pub struct MemoryStore {
     map: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+pub struct PersistentStore {
+    wal_path: os::Path,
+}
+
+impl KVService for PersistentStore {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn put(&mut self, key: &[u8], value: &[u8]) {
+        todo!()
+    }
+
+    fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+        None
+    }
+
+    fn delete(&mut self, key: &[u8]) {
+        todo!()
+    }
 }
 
 trait KVService {
@@ -11,9 +34,9 @@ trait KVService {
     fn delete(&mut self, key: &[u8]);
 }
 
-impl KVService for KVStore {
+impl KVService for MemoryStore {
     fn new() -> Self {
-        KVStore {
+        MemoryStore {
             map: HashMap::new(),
         }
     }
@@ -37,21 +60,21 @@ mod tests {
 
     #[test]
     fn test_put() {
-        let mut store = KVStore::new();
+        let mut store = MemoryStore::new();
         store.put(b"key", b"value");
         assert_eq!(store.get(b"key"), Some(b"value".as_slice()));
     }
 
     #[test]
     fn test_get() {
-        let mut store = KVStore::new();
+        let mut store = MemoryStore::new();
         store.put(b"key", b"value");
         assert_eq!(store.get(b"key"), Some(b"value".as_slice()));
     }
 
     #[test]
     fn test_delete() {
-        let mut store = KVStore::new();
+        let mut store = MemoryStore::new();
         store.put(b"key", b"value");
         store.delete(b"key");
         assert_eq!(store.get(b"key"), None);
